ef2faecbdf803e8dae2e088ee37458b0c4845f31
Avijit Karmaker <ajkacca@gmail.com>
2023-08-07 11:47:39 +0300

    update media placeholder height

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '7d33cd0d7212865955f8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '30f4e1a6867b69ee00cc');
